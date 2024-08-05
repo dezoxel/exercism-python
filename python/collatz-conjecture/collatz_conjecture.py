17d6754d93434853cd9c735181779986340ecc2a
Dez Oxel <dezoxel@gmail.com>
2024-08-05 18:24:09 -0400

    Collatz Conjecture task completed

---
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,10 @@
+def steps(n):
+    if n <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    steps = 0
+    while n != 1:
+        n = n // 2 if n % 2 == 0 else 3*n+1
+        steps += 1
+    
+    return steps
